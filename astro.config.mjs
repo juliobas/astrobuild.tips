36292669793a4507be394e0da7dfba6dedc7bee5
Julio Bastidas <juliobas@gmail.com>
2024-04-12 10:49:24 -0400

    terminando el proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,17 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://astrobuilds.tips',
     integrations: [
         taildwind(),
         mdx(),
         react(),
         vue(),
         svelte(),
+        sitemap(),
     ]
 });
