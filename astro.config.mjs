394a50ce701f0fd280222cc30e9ec914020143d1
Julio Bastidas <juliobas@gmail.com>
2024-04-12 09:17:45 -0400

    agregando componente vue y svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 import taildwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         taildwind(),
         mdx(),
-        react()
+        react(),
+        vue(),
+        svelte(),
     ]
 });
